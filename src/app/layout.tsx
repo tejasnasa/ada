e15480109fdfe483bfef30d43c238cc172143bcb
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2025-02-06 22:37:54 +0530

    Add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import ThemeWrapper from "@/components/theme-wrapper";
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
       >
         <ThemeWrapper>
           {children}
+          <Analytics />
           <Toaster />
         </ThemeWrapper>
       </body>
