0315ef7f9e0fbd2d160b283ff32ad899cf175581
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2025-03-14 15:40:27 +0530

    Create persistant store for settings in local storage

--- src/context/compiler-context.ts
+++ src/context/compiler-context.ts
@@ -1,6 +1,7 @@
 import codeTypeArray from "@/lib/data";
 import Axios from "axios";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 type ThemeType =
   | "vs"
@@ -35,72 +36,79 @@ interface CompilerState {
   setCodingType: (value: number) => void;
 }
 
-export const useCompilerStore = create<CompilerState>((set, get) => ({
-  userCode: codeTypeArray[0].defaultCode,
-  userInput: "",
-  userOutput: {
-    code: "",
-    isError: false,
-  },
-  loading: false,
-  theme: "customDarkTheme",
-  font: 20,
-  codingType: 0,
+export const useCompilerStore = create<CompilerState>()(
+  persist(
+    (set, get) => ({
+      userCode: codeTypeArray[0].defaultCode,
+      userInput: "",
+      userOutput: {
+        code: "",
+        isError: false,
+      },
+      loading: false,
+      theme: "customDarkTheme",
+      font: 20,
+      codingType: 0,
 
-  setUserCode: (code) => set({ userCode: code }),
-  setUserInput: (input) => set({ userInput: input }),
-  setUserOutput: (output) => set({ userOutput: output }),
-  setLoading: (loading) => set({ loading }),
-  setTheme: (theme) => set({ theme }),
-  setFont: (value) => set({ font: value }),
-  setCodingType: (value) => set({ codingType: value }),
+      setUserCode: (code) => set({ userCode: code }),
+      setUserInput: (input) => set({ userInput: input }),
+      setUserOutput: (output) => set({ userOutput: output }),
+      setLoading: (loading) => set({ loading }),
+      setTheme: (theme) => set({ theme }),
+      setFont: (value) => set({ font: value }),
+      setCodingType: (value) => set({ codingType: value }),
 
-  compileCode: async (codingType: number) => {
-    const { userCode, userInput, setUserOutput, setLoading } = get();
+      compileCode: async (codingType: number) => {
+        const { userCode, userInput, setUserOutput, setLoading } = get();
 
-    setLoading(true);
-    try {
-      const config = {
-        method: "post",
-        url: "https://emkc.org/api/v2/piston/execute",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        data: {
-          language: "c++",
-          version: "10.2.0",
-          files: [
-            {
-              name: "main",
-              content:
-                codeTypeArray[codingType].preCode +
-                userCode +
-                codeTypeArray[codingType].postCode,
+        setLoading(true);
+        try {
+          const config = {
+            method: "post",
+            url: "https://emkc.org/api/v2/piston/execute",
+            headers: {
+              "Content-Type": "application/json",
             },
-          ],
-          stdin: userInput,
-        },
-      };
+            data: {
+              language: "c++",
+              version: "10.2.0",
+              files: [
+                {
+                  name: "main",
+                  content:
+                    codeTypeArray[codingType].preCode +
+                    userCode +
+                    codeTypeArray[codingType].postCode,
+                },
+              ],
+              stdin: userInput,
+            },
+          };
 
-      const res = await Axios(config);
+          const res = await Axios(config);
 
-      const data = {
-        code: res.data.run.stdout || res.data.run.stderr,
-        isError: res.data.run.stderr ? true : false,
-      };
+          const data = {
+            code: res.data.run.stdout || res.data.run.stderr,
+            isError: res.data.run.stderr ? true : false,
+          };
 
-      setUserOutput(data);
-    } catch (error) {
-      const data = {
-        code: `Error: ${
-          error instanceof Error ? error.message : "Unknown error"
-        }`,
-        isError: true,
-      };
-      setUserOutput(data);
-      console.log(error);
-    } finally {
-      setLoading(false);
+          setUserOutput(data);
+        } catch (error) {
+          const data = {
+            code: `Error: ${
+              error instanceof Error ? error.message : "Unknown error"
+            }`,
+            isError: true,
+          };
+          setUserOutput(data);
+          console.log(error);
+        } finally {
+          setLoading(false);
+        }
+      },
+    }),
+    {
+      name: "compiler-state",
     }
-  },
-}));
+  )
+);
